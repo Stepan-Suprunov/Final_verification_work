61f3e46c370585d8379ebc313d22e40847a923d3
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-07-26 13:40:12 +0300

    Add new array

--- Program.cs
+++ Program.cs
@@ -4,3 +4,20 @@ for (int i = 0; i < array.Length; i++)
 {
     Console.Write(array[i] + " ");
 }
+
+int count = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j].Length <= 3) count++;
+}
+
+string[] newArray = new string[count];
+int countNew = 0;
+for (int k = 0; k < array.Length; k++)
+{
+    if(array[k].Length <= 3)
+    {
+        newArray[countNew] = array[k];
+        countNew++;
+    }
+}
